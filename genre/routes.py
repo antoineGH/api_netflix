ab3b1e10d0fc1f2ed935c0af65f25703d52ef76a
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-28 19:02:33 +0800

    get genre movie & tv

--- genre/routes.py
+++ genre/routes.py
@@ -1,16 +1,25 @@
 from flask import Blueprint, jsonify
 from flask import request 
 import requests
-from genre.utils import getGenreList
+from genre.utils import getGenreMovieList, getGenreTVList
 
 genre = Blueprint('genre', __name__)
 
 @genre.route('/api/genre/movie/list', methods=['GET'])
-def getGenreListMain():
+def getGenreListMovie():
     if 'language' in request.args:
         language = requests.args.get('language')
     else:
         language='en-US'
-    genre_list = getGenreList(language)
+    genre_list = getGenreMovieList(language)
+    return jsonify(genre_list)
+
+@genre.route('/api/genre/tv/list', methods=['GET'])
+def getGenreListTV():
+    if 'language' in request.args:
+        language = requests.args.get('language')
+    else:
+        language='en-US'
+    genre_list = getGenreTVList(language)
     return jsonify(genre_list)
 
--- genre/utils.py
+++ genre/utils.py
@@ -3,9 +3,14 @@ import requests
 import urllib.parse
 from auth.BearerAuth import BearerAuth
 
-def getGenreList(language):
+def getGenreMovieList(language):
     encoded = urllib.parse.quote(language)
     url = f'https://api.themoviedb.org/3/genre/movie/list?language={encoded}'
     response = requests.get(url, auth=BearerAuth(current_app.config['TMDB_BEARER']))
     return response.json()
 
+def getGenreTVList(language):
+    encoded = urllib.parse.quote(language)
+    url = f'https://api.themoviedb.org/3/genre/tv/list?language={encoded}'
+    response = requests.get(url, auth=BearerAuth(current_app.config['TMDB_BEARER']))
+    return response.json()
