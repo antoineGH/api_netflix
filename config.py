1ae0b72edc085d943cee9006b6e1fa31b96a56d6
Antoine Ratat <antoine.ratat@gmail.com>
2022-01-07 17:42:51 +0800

    update config env var

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
-# import json
 import datetime
 import os 
 
-# with open('config.json') as config_file:
-#     config = json.load(config_file)
-
 class Config:
-    # SECRET_KEY = config.get('SECRET_KEY')
-    # TMDB_BEARER = config.get('TMDB_BEARER')
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_DATABASE')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
