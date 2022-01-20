d7c6d0492fc63255017bc4219d9b1bcf4ac57aac
Antoine Ratat <antoine.ratat@gmail.com>
2022-01-21 02:33:11 +0800

    return movie as object

--- movie/utils.py
+++ movie/utils.py
@@ -18,7 +18,7 @@ def getMovie(movie_id):
     movie = Movie.query.get(movie_id)
     if not movie: 
         return jsonify({"msg": "Movie not found"}), 404
-    return jsonify(movie=movie.serialize)
+    return jsonify(movie.serialize)
 
 def postMovie(tmdb_id, list_id):
     movies = Movie.query.filter_by(list_id=list_id).all()
@@ -29,7 +29,7 @@ def postMovie(tmdb_id, list_id):
     db.session.add(movie)
     try:
         db.session.commit()
-        return jsonify(movie=movie.serialize)
+        return jsonify(movie.serialize)
     except:
         db.session.rollback()
         return jsonify({"msg": "Couldn't add movie to DB"}), 400
