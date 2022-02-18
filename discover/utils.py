219492cf9ffed8122d5b27aa52b832463d9aad16
Antoine Ratat <antoine.ratat@gmail.com>
2022-02-18 15:35:39 +0800

    update url discover tmbd api

--- discover/utils.py
+++ discover/utils.py
@@ -3,6 +3,16 @@ import requests
 from auth.BearerAuth import BearerAuth
 
 def getDiscoverMedia(type_media, language, sort_by, year, with_genres, page):
-    url = f'https://api.themoviedb.org/3/discover/{type_media}?language={language}&sort_by={sort_by}&year={year}&with_genres={with_genres}&include_adult=false&page={page}'
+    if year:
+        year= f'&year={year}'
+    else:
+        year= ''
+
+    if with_genres:
+        with_genres= f'&with_genres={with_genres}'
+    else:
+        with_genres= ''
+
+    url = f'https://api.themoviedb.org/3/discover/{type_media}?language={language}&with_original_language={language[:2]}&sort_by={sort_by}{year}{with_genres}&include_adult=false&page={page}'
     response = requests.get(url, auth=BearerAuth(current_app.config['TMDB_BEARER']))
     return response.json()
